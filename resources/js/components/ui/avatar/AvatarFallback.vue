c028c544e421ec61e317a6dc35b79ea0cc4fcfce
hopewithoute <anggi.wibiyanto@gmail.com>
2026-04-13 21:47:10 +0700

    feat(ui): add command and avatar components

---
+++ resources/js/components/ui/avatar/AvatarFallback.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { AvatarFallback } from 'reka-ui'
+import { cn } from '@/lib/utils'
+
+const props = defineProps({
+    delayMs: { type: Number, required: false },
+    asChild: { type: Boolean, required: false },
+    as: { type: null, required: false },
+    class: { type: null, required: false },
+})
+</script>
+
+<template>
+    <AvatarFallback
+        v-bind="props"
+        :class="
+            cn('bg-muted flex h-full w-full items-center justify-center rounded-full', props.class)
+        "
+    >
+        <slot />
+    </AvatarFallback>
+</template>
