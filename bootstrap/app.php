fae40f45a9c300c5d8f506b30a56ad10597c07e6
hopewithoute <anggi.wibiyanto@gmail.com>
2026-04-13 21:42:01 +0700

    chore: add laravel entry point and bootstrap

---
+++ bootstrap/app.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Middleware\ContextualRoleMiddleware;
+use App\Http\Middleware\EnsureWorkspaceSelected;
+use App\Http\Middleware\HandleInertiaRequests;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware): void {
+        // Register custom middleware aliases
+        $middleware->alias([
+            'role' => ContextualRoleMiddleware::class,
+            'ensure_workspace_selected' => EnsureWorkspaceSelected::class,
+        ]);
+
+        // Append middleware to web group
+        $middleware->web(append: [
+            HandleInertiaRequests::class,
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
