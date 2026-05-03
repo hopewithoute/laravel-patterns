c84b97097c2444287e060b835e198fba02a849d9
hopewithoute <anggi.wibiyanto@gmail.com>
2026-05-03 21:38:44 +0700

    refactor: remove AI backend infrastructure and controllers

--- app/AI/Exceptions/AiToolException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\AI\Exceptions;
-
-use RuntimeException;
-
-class AiToolException extends RuntimeException {}
--- app/AI/Runtime/Enums/PreflightStatus.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\AI\Runtime\Enums;
-
-enum PreflightStatus: string
-{
-    case Allow = 'allow';
-    case Reject = 'reject';
-}
